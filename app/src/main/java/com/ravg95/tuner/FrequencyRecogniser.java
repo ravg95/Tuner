69db5848bee910fafeb6898458213a0533e9614e
Rafal <rafgeb@gmail.com>
2017-12-18 12:44:10 +0100

    fft

--- app/src/main/java/com/ravg95/tuner/FrequencyRecogniser.java
+++ app/src/main/java/com/ravg95/tuner/FrequencyRecogniser.java
@@ -2,24 +2,22 @@ package com.ravg95.tuner;
 
 
 import android.media.AudioFormat;
-import android.media.AudioManager;
 import android.media.AudioRecord;
 
 import android.media.MediaRecorder;
-import android.media.audiofx.Visualizer;
-import android.util.Log;
 
 
 public final class FrequencyRecogniser {
+
+    private final static int SAMPLE_RATE = 44100;
     public static void listen(DoublePointer frequency) {
         int audioSource = MediaRecorder.AudioSource.MIC;
-        int sampleRateInHz = 44100;
         int channelConfig = AudioFormat.CHANNEL_IN_MONO;
         int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
-        int bufferSizeInBytes = AudioRecord.getMinBufferSize(sampleRateInHz, channelConfig, audioFormat);
+        int bufferSizeInBytes = AudioRecord.getMinBufferSize(SAMPLE_RATE, channelConfig, audioFormat);
 
 
-        AudioRecord record = new AudioRecord(audioSource, sampleRateInHz, channelConfig, audioFormat, bufferSizeInBytes);
+        AudioRecord record = new AudioRecord(audioSource, SAMPLE_RATE, channelConfig, audioFormat, bufferSizeInBytes);
 
         if (record.getState() == AudioRecord.STATE_INITIALIZED) {
             record.startRecording();
@@ -33,11 +31,32 @@ public final class FrequencyRecogniser {
             short[] buffer = new short[bufferSizeInBytes / 2];
             int readBytes = record.read(buffer, 0, bufferSizeInBytes / 2);
             if (readBytes >= 0) {           // if readBytes < 0, an error has occured while reading
-               //1 frequency.setValue(FFT.analyze(buffer, readBytes));
+               frequency.setValue(analyze(buffer, readBytes));
             }
 
         }
         while (record.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING );
+    }
+
+    private static double analyze(short[] buffer, int readBytes) {
+        FFT fft = new FFT(readBytes);
+        double[] re = new double[readBytes];
+        double[] im = new double[readBytes];
+        for(int i=0; i<readBytes; i++){
+            re[i] = (double) buffer[i];
+            im[i] = 0;
+        }
+        fft.fft(re, im);
+        double highestPeak = 0;
+        int indexOfHihgestPeak=0;
+        for(int i=0; i<readBytes; i++) {
+            double magnitude = Math.sqrt(re[i]*re[i]+im[i]*im[i]);
+            if(magnitude > highestPeak) {
+                highestPeak = magnitude;
+                indexOfHihgestPeak = i;
+            }
+        }
+        return indexOfHihgestPeak * SAMPLE_RATE / readBytes;
     }
 
 }
